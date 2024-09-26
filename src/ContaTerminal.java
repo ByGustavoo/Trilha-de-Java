b343dcd3668c40b6d8f56028590d960e7e991181
Gustavo <gustavo.ccx@hotmail.com>
2024-09-25 21:55:02 -0300

    INT-5
    - Desafio do Simular uma Conta Bancária Através do Terminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n*****************************");
+        System.out.println("Por favor, digite seu Número:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("\nPor favor, digite o número da sua Agência");
+        int numeroAgencia = scanner.nextInt();
+
+        System.out.println("\nPor favor, digite o seu Nome");
+        String nomeCliente = scanner.next();
+
+        System.out.println("\nPor favor, digite seu Saldo");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, " +
+                "conta %d e seu saldo de R$ %f já está disponível para saque.", nomeCliente, numeroAgencia, numeroConta, saldoConta);
+    }
+}
