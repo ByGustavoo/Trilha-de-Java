5591ad03396fe655e319d3b189bf5f2a5ce8ca54
Gustavo <gustavo.ccx@hotmail.com>
2024-09-27 22:06:48 -0300

    INT-6
    - Desafio de Controle de Fluxo

---
+++ src/Contador.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("\n****************************");
+        System.out.println("Digite o primeiro Número: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo Número: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contarNumeros(parametroUm, parametroDois);
+        } catch (Exception ex) {
+            throw new ParametrosInvalidosException("ERRO! O primeiro número não pode ser maior que o segundo número!", ex);
+        }
+    }
+
+    static void contarNumeros(int numeroUm, int numeroDois) throws ParametrosInvalidosException {
+
+        if (numeroUm > numeroDois) {
+            throw new ParametrosInvalidosException("ERRO! O primeiro número não pode ser maior que o segundo número!");
+        }
+
+        int contagem = numeroDois - numeroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número: " + i);
+        }
+    }
+}
---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+public class ParametrosInvalidosException extends RuntimeException {
+
+    public ParametrosInvalidosException(String mensagem, Throwable ex) {
+        super(mensagem, ex);
+    }
+
+
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
